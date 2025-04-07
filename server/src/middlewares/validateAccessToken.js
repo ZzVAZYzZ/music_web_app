942ce15c244c91095d9707022bb5edc22458d47a
Vazy <86739701+ZzVAZYzZ@users.noreply.github.com>
2025-04-08 02:38:45 +0700

    fix expired bug

--- server/src/middlewares/validateAccessToken.js
+++ server/src/middlewares/validateAccessToken.js
@@ -1,38 +1,40 @@
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+const util = require("util");
+
+const verifyJwt = util.promisify(jwt.verify);
 
 const validateAccessToken = asyncHandler(async (req, res, next) => {
   try {
     let authHeader = req.headers.Authorization || req.headers.authorization;
-    if (authHeader && authHeader.startsWith("Bearer")) {
-      let token = authHeader.split(" ")[1];
-      
-      if (!token) {
-        res.status(401);
-        throw new Error("User is not authorized or token is missing!");
-      }
-      //   verify token
-      jwt.verify(token, process.env.JWT_SECRET_KEY, async (err, decoded) => {
-        if (err) {
-          res.status(401);
-          throw new Error("User is not authorized");
-        } else {
-          const email = decoded.user.email;
-          
-          const user = await User.findOne({email})
-          req.user = user;
-          
-          next();
-        }
-      });
-    } else {
+    if (!authHeader || !authHeader.startsWith("Bearer")) {
       res.status(400);
       throw new Error("Access Token not found");
     }
+
+    let token = authHeader.split(" ")[1];
+    if (!token) {
+      res.status(401);
+      throw new Error("User is not authorized or token is missing!");
+    }
+
+    // ✅ Dùng await để xử lý lỗi token hết hạn đúng cách
+    const decoded = await verifyJwt(token, process.env.JWT_SECRET_KEY);
+
+    const email = decoded.user.email;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(404);
+      throw new Error("User not found");
+    }
+
+    req.user = user;
+    next();
   } catch (error) {
-    res.status(500);
-    throw new Error("An error occured: ", error);
+    res.status(401);
+    next(error);
   }
 });
 
